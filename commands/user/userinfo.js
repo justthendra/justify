0640b04cfb09ec84a17535a5f35e013a4bbce605
Thendra <hasan.erdem211@gmail.com>
2024-05-05 13:02:33 +0300

    Update userinfo.js

--- commands/user/userinfo.js
+++ commands/user/userinfo.js
@@ -6,22 +6,22 @@ module.exports = {
   aliases: ['bilgi'],
   run: async (client, message) => {
 
-    let eleman = message.mentions.users.first() || message.author;
-    if(!eleman) return message.reply("You must tag a person.")
-    const eleman2 = message.guild.members.cache.get(eleman.id)
+    let user = message.mentions.users.first() || message.author;
+    if(!user) return message.reply("You must tag a person.")
+    const memberr = message.guild.members.cache.get(user.id)
 
 
         const emb = new EmbedBuilder()
-        .setAuthor({ name: `${eleman.tag}`, iconURL: eleman.displayAvatarURL()})
+        .setAuthor({ name: `${user.tag}`, iconURL: user.displayAvatarURL()})
         .setDescription("İşte kullanıcın bilgileri.")
         .addFields(
-            { name: "Ad ve etiket", value: `${eleman.tag}`, inline: true},
-            { name: "ID", value: eleman.id, inline: true},
+            { name: "Ad ve etiket", value: `${user.tag}`, inline: true},
+            { name: "ID", value: user.id, inline: true},
             //{ name: "Status", value: eleman.user.presence.status, inline: true},
-            { name: "Avatar", value: `**[JPG](${eleman.displayAvatarURL().replace(eleman.displayAvatarURL().includes(".gif") ? ".gif" : ".png", ".jpg")})**`, inline: true},
-            { name: "Roller", value: `${eleman2.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`, inline: true},
-            { name: "Sunucuya giriş tarihi", value: `${new Date(eleman2.joinedTimestamp).toLocaleDateString()} | ${new Date(eleman2.joinedTimestamp).toLocaleTimeString()}`, inline: true},
-            { name: "Hesap oluşturma tarihi", value: `${new Date(eleman.createdTimestamp).toLocaleDateString()} | ${new Date(eleman.createdTimestamp).toLocaleTimeString()}`, inline: true}
+            { name: "Avatar", value: `**[JPG](${user.displayAvatarURL().replace(user.displayAvatarURL().includes(".gif") ? ".gif" : ".png", ".jpg")})**`, inline: true},
+            { name: "Roller", value: `${member.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`, inline: true},
+            { name: "Sunucuya giriş tarihi", value: `${new Date(member.joinedTimestamp).toLocaleDateString()} | ${new Date(member.joinedTimestamp).toLocaleTimeString()}`, inline: true},
+            { name: "Hesap oluşturma tarihi", value: `${new Date(user.createdTimestamp).toLocaleDateString()} | ${new Date(user.createdTimestamp).toLocaleTimeString()}`, inline: true}
         )
         .setFooter({
             text: `Requested by ${message.author.username}`,
